6f745bf56ad2ec0c2af196920692e28d2a2b3549
Alexander <mardasov.alex@gmail.com>
2020-04-12 21:35:25 +0300

    TicTocToe game class

--- src/TicTacToe2.java
+++ src/TicTacToe2.java
@@ -78,16 +78,18 @@ public class TicTacToe2 {
         map[x][y] = DOT_X;
     }
 
-    private static void computerTurn () {
+    private static void computerTurn() {
+        int nextTurn = 0;
+        int[] axisX = new int[9];
+        int[] axisY = new int[9];
         int x = -1;
         int y = -1;
-        if(SILLY_MODE) {
+        if (SILLY_MODE) {
             do {
                 x = random.nextInt(SIZE);
                 y = random.nextInt(SIZE);
-            } while (!isCellValid(x,y));
-    }
-        else {
+            } while (!isCellValid(x, y));
+        } else {
             // Первая часть, выбираем первую свободную клетку слева направо сверху вниз
             if (turnCounter == 0) {
                 here:
@@ -106,30 +108,39 @@ public class TicTacToe2 {
 
             // Вторая часть, обходим клетки если соседняя клетка содержит 0, то ставим в текущую
             if (turnCounter > 0) {
-                here:
+//               here:
                 for (int i = 0; i < SIZE; i++) {
                     for (int j = 0; j < SIZE; j++) {
                         boolean isLeftTopZero = map[Math.max(0, i - 1)][Math.max(0, j - 1)] == DOT_O;
                         boolean isTopZero = map[Math.max(0, i - 1)][j] == DOT_O;
-                        boolean isTopRightZero = map[Math.max(0, i - 1)][Math.min(2, j+ 1)] == DOT_O;
+                        boolean isTopRightZero = map[Math.max(0, i - 1)][Math.min(2, j + 1)] == DOT_O;
                         boolean isLeftZero = map[i][Math.max(0, j - 1)] == DOT_O;
                         boolean isRightZero = map[i][Math.min(2, j + 1)] == DOT_O;
-                        boolean isBottomLeftZero = map[Math.min(2, i + 1)][Math.max(0,j - 1)] == DOT_O;
+                        boolean isBottomLeftZero = map[Math.min(2, i + 1)][Math.max(0, j - 1)] == DOT_O;
                         boolean isBottomZero = map[Math.min(2, i + 1)][j] == DOT_O;
                         boolean isBottomRightZero = map[Math.min(2, i + 1)][Math.min(2, j + 1)] == DOT_O;
 
                         boolean isAnyCellZero = isLeftTopZero || isBottomRightZero || isRightZero || isLeftZero || isBottomZero || isTopZero || isBottomLeftZero || isTopRightZero;
                         if (map[i][j] == DOT_EMPTY && isAnyCellZero) {
-                            x = i;
-                            y = j;
-
-
-                           break here;
+                          /*  x = i;
+                            y = j;*/
+                            axisX[nextTurn] = i;
+                            axisY[nextTurn] = j;
+                            nextTurn++;
+                        /*    System.out.println(axisX[i]);
+                            System.out.println(axisY[j]);*/
+                        
+//                            break here;
                         }
 
                     }
 
                 }
+                int randomNextTurn = random.nextInt(nextTurn);
+                x = axisX[randomNextTurn];
+                y = axisY[randomNextTurn];
+                /*System.out.println(Arrays.toString(axisX));
+                System.out.println(Arrays.toString(axisY));*/
 
 
             }
@@ -138,7 +149,6 @@ public class TicTacToe2 {
 
         System.out.println("Компьютер выбрал ячейку " + (x + 1) + " " + (y + 1));
         map[x][y] = DOT_O;
-
 
 
     }
@@ -192,7 +202,7 @@ public class TicTacToe2 {
     private static boolean checkWin(char playerSymbol) {
         boolean result = false;
         if (
-                        (map[0][0] == playerSymbol && map[0][1] == playerSymbol && map[0][2] == playerSymbol) ||
+                (map[0][0] == playerSymbol && map[0][1] == playerSymbol && map[0][2] == playerSymbol) ||
                         (map[1][0] == playerSymbol && map[1][1] == playerSymbol && map[1][2] == playerSymbol) ||
                         (map[2][0] == playerSymbol && map[2][1] == playerSymbol && map[2][2] == playerSymbol) ||
                         (map[0][0] == playerSymbol && map[1][0] == playerSymbol && map[2][0] == playerSymbol) ||
@@ -203,7 +213,7 @@ public class TicTacToe2 {
             result = true;
         }
 
-            return result;
+        return result;
     }
 
 
